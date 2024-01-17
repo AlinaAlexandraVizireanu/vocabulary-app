61d66b0d9fd8e6157bc5bc21939bb83d3e9abf7e
Alina Vizireanu <vizireanualina15@yahoo.com>
2024-01-17 12:19:41 +0200

    implement dark mode

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const form = document.querySelector("form");
 const input = document.querySelector("#text");
 const container = document.querySelector(".container");
+const containerLogo = document.querySelector(".logo_container-book");
 const mainContent = document.querySelector(".main_content");
 const line = document.querySelector(".main_content-line");
 const meaning = document.querySelector(".main_content-meaning");
+const toggleBtn = document.querySelector(".toggleBtn");
+const toggleBtnLogo = document.querySelector(".toggle_container-logo");
+
 const displayWord = document.createElement("h1");
 const displayMeaning = document.createElement("p");
 
@@ -39,5 +43,19 @@ form.addEventListener("submit", function (e) {
       .catch((err) => {
         console.log(err);
       });
+  }
+});
+
+toggleBtn.addEventListener("click", function () {
+  if (!document.body.classList.contains("dark-mode")) {
+    document.body.classList.add("dark-mode");
+    line.classList.add("main_content-line-dark");
+    containerLogo.src = "./images/menu_book_white_48dp.svg";
+    toggleBtnLogo.src = "./images/light_mode_white_36dp.svg";
+  } else {
+    document.body.classList.remove("dark-mode");
+    line.classList.remove("main_content-line-dark");
+    containerLogo.src = "./images/menu_book_black_48dp.svg";
+    toggleBtnLogo.src = "./images/dark_mode_black_36dp.svg";
   }
 });
